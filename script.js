52708600254e1a41a887433302af1da72fa1b463
mohamedx97 <mohamedmailone@gmail.com>
2025-01-06 23:01:56 +0200

    add playRound function to play one round and remove unneeded varriable from getHumanChoice function

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function getComputerChoice(){
     }
 }
 
-function getHumanChoice(userChoice){
+function getHumanChoice(){
     userChoice = prompt("your turn!, write:Rock, Paper or Scissors").toLowerCase();
     switch (userChoice){
         case "rock":
@@ -24,3 +24,47 @@ function getHumanChoice(userChoice){
             return "Error!";
     }
 }
+
+function playRound(humanChoice,computerChoice){
+    
+    if (humanChoice === computerChoice){
+        console.log("tie!")
+    }
+
+    else if (humanChoice === "Error!"){
+        computerScore ++;
+        console.log("you have entred a wrong value so pc wins!");
+    }
+
+    else if (humanChoice === "Rock" && computerChoice === "Paper"){
+        computerScore ++;
+        console.log("pc wins Paper beats Rock");
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Scissors"){
+        computerScore ++;
+        console.log("pc wins Paper beats Scissors");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "Rock"){
+        computerScore ++;
+        console.log("pc wins scissors beats Rock");
+    }
+    else if (humanChoice === "Rock" && computerChoice === "Scissors"){
+        humanScore ++;
+        console.log("you wins Rock beats Scissors");
+    }
+    else if (humanChoice === "Paper" && computerChoice === "Rock"){
+        humanScore ++;
+        console.log("you wins Paper beats Rock");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "Paper"){
+        humanScore ++;
+        console.log("you wins scissors beats Paper");
+    }
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+humanSelection = getHumanChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
